d722623ce571d2c028e678ec90bb1d12c8e7405c
fhazuki <ktc23a31e0016@edu.kyoto-tech.ac.jp>
2023-09-08 13:04:58 +0900

    0908

---
+++ snoopy/js/main.js
@@ -0,0 +1,7 @@
+ScrollReveal({reset: true, distance: "60px", duration: 2500, delay: 200 });
+ScrollReveal().reveal(".main-title", {delay: 200, origin: "bottom" });
+ScrollReveal().reveal(".section-title", {delay: 400, origin: "top" });
+ScrollReveal().reveal(".sec-01 .image, .sec-03 .image", {delay: 600, origin: "left" });
+ScrollReveal().reveal(".sec-02 .image", {delay: 600, origin: "right" });
+ScrollReveal().reveal(".sec-01 .info, .sec-03 .info", {delay: 700, origin: "right" });
+ScrollReveal().reveal(".sec-02 .info", {delay: 700, origin: "left" });
